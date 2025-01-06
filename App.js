d96c6de82a117e7b2c72a0cc463151bf8e958761
Harsha6463 <157800250+Harsha6463@users.noreply.github.com>
2025-01-06 13:22:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+import Q4 from './components/Q4';
+import Q3 from './components/Q3'
+import Imagecarousel from './components/Imagecarousel';
+import Factorial from './components/Factorial';
+import Additems from './components/Additems';
+import Q7 from './components/Q7';
+import Q8 from './components/Q8'
+import Formdata from './components/Formdata';
+
+
+
+function App() {
+  return (
+    <div className="App">
+  <Q4></Q4>
+  <Q3></Q3>
+  <Imagecarousel/>
+  <Additems/>
+  <Factorial/>
+  <Q7></Q7>
+  <Q8></Q8>
+  <Formdata></Formdata>
+
+    </div>
+  );
+}
+
+export default App;
